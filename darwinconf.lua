31e5f3ca68767333b72055407e9456411c87c4f5
SamuelHenriqueDeMoraisVitrio <samuelhenriquemoraisvitrio@gmail.com>
2025-06-25 15:23:31 -0300

    tt

---
+++ darwinconf.lua
@@ -0,0 +1,19 @@
+local all = {
+  [[return (function()    
+      local Private = {};
+      local Berrante = {};
+  ]]
+    }
+  local files = darwin.dtw.list_files_recursively("src",true)
+
+  for _, file in ipairs(files) do
+    all[#all  +1 ] = darwin.dtw.load_file(file) .."\n"
+  end
+all[#all + 1] = [[
+
+    return Berrante;
+end)()]]
+
+
+local result = table.concat(all, "\n")
+darwin.dtw.write_file("LuaBerrante.lua", result)
