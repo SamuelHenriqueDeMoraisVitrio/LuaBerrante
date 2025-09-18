434fa6d5e023f7787b1285ae74e1249f6fff79f2
SamuelHenriqueDeMoraisVitrio <samuelhenriquemoraisvitrio@gmail.com>
2025-09-18 16:38:08 -0300

    Dtt

--- darwinconf.lua
+++ darwinconf.lua
@@ -2,13 +2,16 @@ local all = {
   [[return (function()    
       local Private = {};
       local Berrante = {};
+
   ]]
     }
-  local files = darwin.dtw.list_files_recursively("src",true)
+local types_coontent = darwin.dtw.load_file("types.lua")
+local files = darwin.dtw.list_files_recursively("src",true)
 
-  for _, file in ipairs(files) do
-    all[#all  +1 ] = darwin.dtw.load_file(file) .."\n"
-  end
+all[#all + 1] = types_coontent .. "\n"
+for _, file in ipairs(files) do
+  all[#all + 1] = darwin.dtw.load_file(file) .."\n"
+end
 all[#all + 1] = [[
 
     return Berrante;
